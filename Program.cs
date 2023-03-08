c49d4b65f5c3d8286ef9365acc0d384a657ceeaa
arielcollatti <colatti2017@gmail.com>
2023-03-08 13:34:56 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿Console.WriteLine("musica preferida"); 
+Console.WriteLine ("");
+
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine ("Muleque de vila-Projota");
+Console.ResetColor();
+Console.WriteLine ("");
+
+Console.WriteLine ("Eu falei que era uma questão de tempo"); 
+Console.WriteLine ("E tudo ia mudar, e eu lutei"); 
+Console.WriteLine ("Vários me disseram que eu nunca ia chegar, duvidei"); 
+Console.WriteLine ("Lembra da ladeira, meu?"); 
+Console.WriteLine ("Toda Sexta-feira meu melhor amigo é Deus e o segundo melhor sou eu"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Eu tanto quis, tanto fiz, tanto fui feliz"); 
+Console.WriteLine ("Eu canto Xis, canto Péricles, canto Elis"); 
+Console.WriteLine ("Torcedor do Santos, desse pão e circo eu também quis"); 
+Console.WriteLine ("Não sei feliz, mas geral merece não ser infeliz");  
+Console.WriteLine (""); 
+
+Console.WriteLine ("Prosperei com o suor do meu trabalho"); 
+Console.WriteLine ("Me guardei, lutei sem buscar atalho"); 
+Console.WriteLine ("Ou sem pisar em ninguém"); 
+Console.WriteLine ("Sem roubar também, então sei"); 
+Console.WriteLine ("Que hoje o meu nome é Foda e meu sobrenome é Pra Caralho"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Deus olhou pra mim, disse assim, escuta neguin"); 
+Console.WriteLine ("Pegue esse caderno e escreve em cada folha até o fim"); 
+Console.WriteLine ("Eu disse Senhor, sou tão tímido, sinto mó pavor"); 
+Console.WriteLine ("Só subir no palco a perna congelou"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Mas rodei o Brasil, CD na mochila foi cinquenta mil"); 
+Console.WriteLine ("Mão em mão, na rodoviária passando mó frio"); 
+Console.WriteLine ("Quem viu, viu, Curitiba meu tesouro, foi estouro"); 
+Console.WriteLine ("Vinte e cinco mil, tio, DVD de ouro"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Triunfo bombou, Leandro estourou, Michel prosperou"); 
+Console.WriteLine ("Dei valor, só trabalhador, homens de valor"); 
+Console.WriteLine ("Minha cor não me atrapalhou, só me abençoou"); 
+Console.WriteLine ("Quem falou que era moda, hoje felizmente se calou"); 
+Console.WriteLine (""); 
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine ("Vai, vai lá, não tenha medo do pior"); 
+Console.WriteLine ("Eu sei que tudo vai mudar"); 
+Console.WriteLine ("Você vai transformar o mundo ao seu redor"); 
+Console.WriteLine ("Mas não vacila, moleque de vila, moleque de vila, moleque de vila"); 
+Console.WriteLine ("Não vacila, moleque de vila, moleque de vila, moleque de vila"); 
+Console.ResetColor();
+Console.WriteLine (""); 
+
+Console.WriteLine ("Já fui vaiado, já fui humilhado, já fui atacado"); 
+Console.WriteLine ("Fui xingado, ameaçado, nunca amedrontado"); 
+Console.WriteLine ("Aplaudido, reverenciado, homenageado"); 
+Console.WriteLine ("Premiado pelos homens, por Deus abençoado"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Avisa o Rony que hoje é nós, não tem show, 'to sem voz"); 
+Console.WriteLine ("Se o Danilo não colar, vou buscar de Cross"); 
+Console.WriteLine ("Se o Marques chegar, grita o Magrão, liga, mó função"); 
+Console.WriteLine ("Tem churrasco, sem fiasco, tira espinha do salão"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Já cantei com o Mano Brown, com Edi Rock e com Helião"); 
+Console.WriteLine ("Com D2, com MV, dei um abraço no Chorão"); 
+Console.WriteLine ("Aprendi fazer freestyle no busão"); 
+Console.WriteLine ("Hoje é o mesmo freestyle, só que a gente faz no fundo do avião"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("E hoje eu acordei chorando porque eu me peguei pensando"); 
+Console.WriteLine ("Será que lá de cima a minha véia segue me olhando?"); 
+Console.WriteLine ("Será que se me olhando, ela ainda 'tá me escutando?"); 
+Console.WriteLine ("Será que me escutando, ela ainda 'tá se orgulhando?"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Hoje tanto faz, putaria 'tá demais"); 
+Console.WriteLine ("Mas ninguém se liga mais, mas ninguém respeita os pais"); 
+Console.WriteLine ("Mas pra mim tanto faz porque ainda tem Racionais"); 
+Console.WriteLine ("Pra quem quer, diferente tem, Oriente e Haikass"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Raps nacionais, rostos diferentes, mesmos ideais"); 
+Console.WriteLine ("Salve, Sabota, e todo Rap sem lorota"); 
+Console.WriteLine ("Os mano gosta de ir no Twitter xingar o Projota"); 
+Console.WriteLine ("Mas trai a mulher e não abraça a mãe, faz uma cota"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Desde os dezesseis 'to aqui, outra vez, vou sorrir"); 
+Console.WriteLine ("Vou cantar, vou seguir"); 
+Console.WriteLine ("Vou tentar, conseguir"); 
+Console.WriteLine ("Se quer falar mal, fala daí"); 
+Console.WriteLine ("Mas meu público grita tão alto que já nem consigo te ouvir"); 
+Console.WriteLine (""); 
+
+Console.WriteLine ("Olha lá o outdoor com o meu nome"); 
+Console.WriteLine ("Me emocionar não me faz ser menos homem"); 
+Console.WriteLine ("Se o diabo amassa o pão, você morre ou você come?"); 
+Console.WriteLine ("Eu não morri e nem comi, eu fiz amizade com a fome"); 
+Console.WriteLine (""); 
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine ("Vai, vai lá, não tenha medo do pior"); 
+Console.WriteLine ("Eu sei que tudo vai mudar"); 
+Console.WriteLine ("Você vai transformar o mundo ao seu redor"); 
+Console.WriteLine ("Mas não vacila, moleque de vila, moleque de vila, moleque de vila"); 
+Console.WriteLine ("Não vacila, moleque de vila, moleque de vila, moleque de vila"); 
+Console.ResetColor();
